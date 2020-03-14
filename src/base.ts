ed6887f59b4973926091e5985da4d1eb5fd09b49
Ian Sibner <sibnerian@gmail.com>
2020-03-13 23:07:17 -0400

    Security and coverage updates

--- src/base.ts
+++ src/base.ts
@@ -1,21 +1,7 @@
 import uuid from 'uuid/v4';
 import { serializeError } from 'serialize-error';
 import { IpcMain, IpcRenderer, WebContents, IpcMessageEvent } from 'electron';
-
-// serializeError requires an Object.entries polyfill on node <= 6.
-// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries#Polyfill
-/* eslint-disable */
-if (!Object.entries) {
-  Object.entries = function(obj) {
-    var ownProps = Object.keys(obj),
-      i = ownProps.length,
-      resArray = new Array(i); // preallocate the Array
-    while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]];
-
-    return resArray;
-  };
-}
-/* eslint-enable */
+import 'object.entries/auto'; // Shim Object.entries. Required to use serializeError.
 
 /**
  * For backwards compatibility, event is the (optional) LAST argument to a listener function.
